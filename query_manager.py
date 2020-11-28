1ab4f18c8ea1576b9dcd54cec3bdfe6196ac1cd5
AhmadKObeid <64746765+AhmadKObeid@users.noreply.github.com>
2020-11-28 23:50:12 +0200

    Update query_manager.py

--- query_manager.py
+++ query_manager.py
@@ -42,7 +42,7 @@ def get_es_results():
                                     {
                                         "query": query,
                                         "fuzziness": "AUTO",
-                                        "operator": "and"
+                                        "operator": "or"
                                     }
                                 }
                             },
