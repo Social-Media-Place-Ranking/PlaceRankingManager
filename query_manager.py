049b57c4bdef423b31de20d3eb96bd15c755861f
Ahmad Obeid <ahmadkobeid96@gmail.com>
2020-12-22 03:52:48 +0200

    optimize code

--- query_manager.py
+++ query_manager.py
@@ -7,18 +7,16 @@ import requests
 import os
 
 app = Flask(__name__)
-es = Elasticsearch(
-    ['https://ef66b61709b64f359a9fb032e3320868.us-central1.gcp.cloud.es.io:9243/'],
-    http_auth=("elastic", "6av14Q56vXkyVDYSdkLJeXzi"),
-)
+
 """
 Flask App Routes
 """
 @app.route("/")
 def welcome():
-    return "Query Manager"
+    return config.get("WELCOMING")
 
-@app.route("/search", methods=["GET"])
+
+@app.route("/search", methods=["GET","POST"])
 def get_es_results():
     """Post Method to get the data from es
 
@@ -26,32 +24,40 @@ def get_es_results():
         [Json]: Ranked Locations
     """
 
-    if request.method =="GET":
+    if request.method == "GET":
         query = request.args.get("query")
         lon = request.args.get("lon")
         lat = request.args.get("lat")
-        res= es.search(
-            index='places', 
-            body ={
-                   "query":{
-                            "match":{
-                               "name":{
-                                  "query":query,
-                                  "fuzziness":"AUTO"
-                               }
-                            }
-                         }
-                      }
-                    ) 
-        places = json.dumps(res['hits']['hits'][:5])
-        reranked_data = requests.post(url = "https://ml-rerank-service.herokuapp.com/re-rank?lon="+str(lon)+"&lat="+str(lat)+"&query="+query, json = places)
+        res = es.search(
+            index='places',
+            body={
+                "query": {
+                    "match": {
+                        "name": {
+                            "query": query,
+                            "fuzziness": "AUTO"
+                        }
+                    }
+                }
+            }
+        )
+        places = {"places" : res['hits']['hits'][:5], "query" : query, "lat" : lat, "lon" : lon}
+        reranked_data = requests.post(url = "https://ml-rerank-service.herokuapp.com/re-rank", json = json.dumps(places)) 
         return (jsonify(reranked_data.json()))
-
+    else:
+        return config.get("WELCOMING")
 """
 Flask App Entry Point (Main)
 """
 if __name__ == "__main__":
-    #load model at app launch
-    port = int(os.environ.get('PORT',5000))
-    app.run(debug=True, use_reloader=False,host='0.0.0.0', port =port)
 
+    with open("config.json") as json_file:
+        # append the json file, to get all of them in on json variable
+        config = json.load(json_file)
+    # login to elasticsearch
+    es = Elasticsearch(
+        [config.get("ES_ENDPOINT")],
+        http_auth=(config.get("ES_USERNAME"), config.get("ES_PASS")),
+    )
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=port)
